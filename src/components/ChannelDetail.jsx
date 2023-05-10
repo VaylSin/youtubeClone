d543c154feaf7744d2720265bab0b678de2c192a
VaylSin <slyb33developer@gmail.com>
2023-05-10 13:35:19 +0200

    clear env file from repo

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,13 +1,16 @@
-import { Box } from '@mui/material'
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import {Videos, ChannelCard} from './'
+import React, { useEffect, useState } from 'react';
+import { Box } from '@mui/material';
+import { useParams } from 'react-router-dom';
+import {Videos, ChannelCard} from './';
 
 const ChannelDetail = () => {
+
   const {id} = useParams();
+
   return (
     <div>{id}</div>
   )
+  
 }
 
 export default ChannelDetail
