992225f943a9018a4b9f908ad26bb1cb806b8755
VaylSin <slyb33developer@gmail.com>
2023-05-10 13:18:54 +0200

    init commit

---
+++ src/components/ChannelDetail.jsx
@@ -0,0 +1,13 @@
+import { Box } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import {Videos, ChannelCard} from './'
+
+const ChannelDetail = () => {
+  const {id} = useParams();
+  return (
+    <div>{id}</div>
+  )
+}
+
+export default ChannelDetail
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,46 @@
+import { Stack } from '@mui/material'
+import React from 'react'
+import { categories } from '../utils/constants'
+
+
+
+const Sidebar = ({selectedCategory, setselectedCategory}) => {
+    return (
+        <Stack
+        direction='row'
+        sx={{
+            overflowY:'hidden',
+            height: {
+                sx: 'auto',
+                md: "95%"
+            },
+            flexDirection: {
+                md: 'column'
+            }
+        }}>
+            {categories.map( category => (
+                <button
+                    key={category.name}
+                    className='category-btn'
+                    onClick={() => setselectedCategory(category.name)}
+                    // attention SX est uniquemnet pour les composants MUI !!! Sinon utiliser Style
+                    style= {{
+                        background: category.name === selectedCategory && '#FC1503',
+                        color: '#fff'
+                    }}
+                >
+                    <span style={{
+                        color: category.name === selectedCategory ?"#fff" : '#FC1503',
+                        marginRight: '15px',
+                        
+                    }}>{category.icon}</span>
+                    <span 
+                    style={{
+                        opacity: category.name === selectedCategory ? '1' : '0.8'
+                    }}>{category.name}</span>
+                </button>
+            ))}
+        </Stack>     )
+}
+
+export default Sidebar
