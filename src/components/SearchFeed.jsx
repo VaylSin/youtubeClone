bea19e3785bc6f4c41306b2b7a571f01d01e80d7
SlyB33 <skvmOmega@gmail.com>
2025-02-22 14:17:34 +0100

    noindex + branding

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,43 +1,42 @@
-import React, { useEffect, useState } from 'react'
-import { Box, Typography } from '@mui/material'
-import {Videos} from './'
-import { fetchFromAPI } from '../utils/fetchFromAPI'
-import { useParams } from 'react-router-dom'
-
+import React, { useEffect, useState } from "react";
+import { Box, Typography } from "@mui/material";
+import { Videos } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { useParams } from "react-router-dom";
 
 const SearchFeed = () => {
+	const [videos, setVideos] = useState([]);
+	const { searchTerm } = useParams();
 
-  const [videos, setVideos] = useState([]);
-  const {searchTerm} = useParams();
+	useEffect(() => {
+		fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) =>
+			setVideos(data.items)
+		);
+	}, [searchTerm]);
 
-  useEffect(() => {
-    fetchFromAPI(`search?part=snippet&q=${searchTerm}`)
-    .then((data) => setVideos(data.items));
-    
-  }, [searchTerm]);
-  
-  return (
-    <Box 
-      p={2}
-      sx={{
-        overflowX: 'auto',
-        height: '90vh',
-        flex: 2
-      }} 
-    >
-      <Typography 
-        variant="h4"
-        fontWeight='bold'
-        mb={2}
-        sx={{
-          color: '#fff'
-        }}
-        >
-        Search results for : <span style={{color: '#F31503'}}> {searchTerm} </span>
-      </Typography>
-      <Videos videos={videos} justifyContent="center" />
-    </Box>
-  )
-}
+	return (
+		<Box
+			p={2}
+			sx={{
+				overflowX: "auto",
+				height: "90vh",
+				flex: 2,
+			}}
+		>
+			<Typography
+				variant="h4"
+				fontWeight="bold"
+				mb={2}
+				sx={{
+					color: "#fff",
+				}}
+			>
+				Search results for :{" "}
+				<span style={{ color: "#F31503" }}> {searchTerm} </span>
+			</Typography>
+			<Videos videos={videos} justifyContent="center" />
+		</Box>
+	);
+};
 
-export default SearchFeed
+export default SearchFeed;
--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,42 +1,33 @@
-import MusicNoteIcon from '@mui/icons-material/MusicNote';
-import HomeIcon from '@mui/icons-material/Home';
-import CodeIcon from '@mui/icons-material/Code';
-import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
-import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
-import LiveTvIcon from '@mui/icons-material/LiveTv';
-import SchoolIcon from '@mui/icons-material/School';
-import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
-import CheckroomIcon from '@mui/icons-material/Checkroom';
-import GraphicEqIcon from '@mui/icons-material/GraphicEq';
-import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
-import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
-import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
+import HomeIcon from "@mui/icons-material/Home";
+import CodeIcon from "@mui/icons-material/Code";
+import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
+import SchoolIcon from "@mui/icons-material/School";
+import GraphicEqIcon from "@mui/icons-material/GraphicEq";
+import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
 
-export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
-
-
+export const logo = "https://skdigit.com/web_banner.png";
 
 export const categories = [
-    { name: 'New', icon: <HomeIcon />, },
-    { name: 'JS Mastery', icon: <CodeIcon />, },
-    { name: 'Decline of the I', icon: <GraphicEqIcon />, },
-    { name: 'Coding', icon: <CodeIcon />, },
-    { name: 'ReactJS', icon: <CodeIcon />, },
-    { name: 'NextJS', icon: <CodeIcon />, },
-    { name: 'Music', icon: <MusicNoteIcon /> },
-    { name: 'Black Metal', icon: <MusicNoteIcon /> },
-    { name: 'Cooking', icon: <SchoolIcon />, },
-    { name: 'Podcast', icon: <GraphicEqIcon />, },
-    { name: 'Movie', icon: <OndemandVideoIcon />, },
-    { name: 'Gaming', icon: <SportsEsportsIcon />, },
-    { name: 'Live', icon: <LiveTvIcon />, },
-    { name: 'Sport', icon: <FitnessCenterIcon />, },
-
+	{ name: "Accueil", icon: <HomeIcon /> },
+	{ name: "Coding", icon: <CodeIcon /> },
+	{ name: "ReactJS", icon: <CodeIcon /> },
+	{ name: "NextJS", icon: <CodeIcon /> },
+	{ name: "Seth", icon: <GraphicEqIcon /> },
+	{ name: "Temple of Baal", icon: <GraphicEqIcon /> },
+	{ name: "Decline of the I", icon: <GraphicEqIcon /> },
+	{ name: "Diablo 4", icon: <SportsEsportsIcon /> },
+	{ name: "Diablo 3", icon: <SportsEsportsIcon /> },
+	{ name: "League Of Legend", icon: <SportsEsportsIcon /> },
+	{ name: "Drummer", icon: <MusicNoteIcon /> },
+	{ name: "Cooking", icon: <SchoolIcon /> },
+	{ name: "Muscu", icon: <FitnessCenterIcon /> },
 ];
 
-export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
-export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
-export const demoVideoUrl = '/video/GDa8kZLNhJ4';
-export const demoChannelTitle = 'JavaScript Mastery';
-export const demoVideoTitle = 'Sly test youtube clone';
-export const demoProfilePicture = 'https://dergipark.org.tr/assets/app/images/buddy_sample.png'
+export const demoThumbnailUrl = "https://i.ibb.co/G2L2Gwp/API-Course.png";
+export const demoChannelUrl = "/channel/UCmXmlB4-HJytD7wek0Uo97A";
+export const demoVideoUrl = "/video/GDa8kZLNhJ4";
+export const demoChannelTitle = "JavaScript Mastery";
+export const demoVideoTitle = "Sly test youtube clone";
+export const demoProfilePicture =
+	"https://dergipark.org.tr/assets/app/images/buddy_sample.png";
